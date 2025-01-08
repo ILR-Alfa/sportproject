4793a6264e3fd69ee13cb45f09f5542845d2b85b
ILR <verbar865@gmail.com>
2025-01-08 16:18:43 +0500

    2

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:MINIwaer12345!@localhost:5432/sport_database?client_encoding=utf8"
+DATABASE_URL = "postgresql://postgres:MINIwaer12345!@localhost:5432/sport_database"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
